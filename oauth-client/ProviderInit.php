30655c3314d64edd055bf4e75cc68c8d3a6c3b8f
alexbaudry0 <alexbaudry0@gmail.com>
2021-07-19 22:53:51 +0200

    fix constructor scope

--- oauth-client/ProviderInit.php
+++ oauth-client/ProviderInit.php
@@ -5,7 +5,10 @@ require "Provider.php" ;
 function init($array) {
 	$providers = [] ;
 	foreach ($array as $provider => $value) {
-		$providers[$provider] = new Provider($value["clientID"],$value["clientSecret"],$value["urlAuth"],$value["urlToken"],$value["urlResult"],$value['redirectUri']) ;
+	    if(!isset($value["scope"]))
+		    $providers[$provider] = new Provider($value["clientID"],$value["clientSecret"],$value["urlAuth"],$value["urlToken"],$value["urlResult"],$value['redirectUri']) ;
+	    else
+            $providers[$provider] = new Provider($value["clientID"],$value["clientSecret"],$value["urlAuth"],$value["urlToken"],$value["urlResult"],$value['redirectUri'],$value['scope']) ;
 	}
 	return $providers ;
 }
